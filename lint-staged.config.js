5513d9d92c92619b3a6393ec3cfc91a13a742dc8
abhishekode <iabhishekn98@gmail.com>
2024-07-19 12:57:08 +0530

    feature/added admin panel

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   './client/**/*.{ts,tsx,js,jsx}': ['eslint --fix', 'prettier --write'],
   './server/**/*.{ts,js}': ['eslint --fix', 'prettier --write'],
-  './admin/**/*.{ts,js}': ['eslint --fix', 'prettier --write'],
+  './admin/**/*.{ts,tsx,js,jsx}': ['eslint --fix', 'prettier --write'],
 };
